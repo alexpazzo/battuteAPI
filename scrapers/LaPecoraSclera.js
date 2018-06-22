1ffdbd60f4fe94f10c895d0050c09614a0ca72f5
Alessandro Gatti <ale.gatti96@gmail.com>
2018-06-23 01:05:37 +0200

    add LaPecoraSclera.getJokesFromPage

--- scrapers/LaPecoraSclera.js
+++ scrapers/LaPecoraSclera.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const cheerio = require('cheerio');
+
 const Scraper = require('./Scraper.js');
 
 /**
@@ -14,18 +16,32 @@ class LaPecoraSclera extends Scraper {
 
     get PAGES() {
         return [{
-            page: 'chuck-norris-facts.php?PA={{PAGINATION}}',
+            page: 'chuck-norris-facts.php?PA={{PAGE}}',
             categories: [Scraper.CATEGORIES.CHUCK_NORRIS]
         }, {
-            page: 'chuck-norris-facts.php?PA={{PAGINATION}}',
+            page: 'chuck-norris-facts.php?PA={{PAGE}}',
             categories: [Scraper.CATEGORIES.FREDDURE]
         }, {
-            page: 'battute-divertenti.php?PA={{PAGINATION}}&genere=1&label=colmi',
+            page: 'battute-divertenti.php?PA={{PAGE}}&genere=1&label=colmi',
             categories: Scraper.CATEGORIES.COLMI
         }, {
-            page: '/battute-divertenti.php?PA={{PAGINATION}}&genere=3&label=differenze',
+            page: '/battute-divertenti.php?PA={{PAGE}}&genere=3&label=differenze',
             categories: Scraper.CATEGORIES.DIFFERENZE
         }];
+    }
+
+    async getJokesFromPage(page, options) {
+        const html = await this._downloadPage(page);
+        const $ = cheerio.load(html);
+        const divs = $('.panel-body > div')
+        const jokes = Array.from(divs)
+            .map(div => div.children[0].data)
+            .map(joke => ({
+                text: joke,
+                categories: [...page.categories],
+                source: 'LaPecoraSclera'
+            }));
+        return jokes;
     }
 
 }
