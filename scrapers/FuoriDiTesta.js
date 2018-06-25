4927fcfb7c83c64e9b3334c2ebb59157ad37ffda
Alessandro Belli <alessandro.belli@softecspa.it>
2018-06-25 14:35:23 +0200

    fix foriditesta PAGES

--- scrapers/FuoriDiTesta.js
+++ scrapers/FuoriDiTesta.js
@@ -5,72 +5,56 @@ const Scraper = require('./Scraper.js');
 
 class FuoriDiTesta extends Scraper {
 
-    static get BASE_URL() {
+    get BASE_URL() {
         return "http://www.fuoriditesta.it/barzellette/";
     }
 
-    static get PAGES() {
+    get PAGES() {
         return [{
-            "AFORISMI": {
-                page: "aforismi-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.AFORISMI]
-            },
-            "ANIMALI": {
-                page: "animali-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.ANIMALI]
-            },
-            "CALCIO": {
-                page: "calcio-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.CALCIO]
-            },
-            "CARABINIERI": {
-                page: "carabinieri-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.CARABINIERI]
-            },
-            "COLMI_E_FREDDURE": {
-                page: "colmi-e-freddure-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.COLMI, Scraper.CATEGORIES.FREDDURE]
-            },
-            "DAL_DOTTORE": {
-                page: "dal-dottore-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.DAL_DOTTORE]
-            },
-            "DONNE": {
-                page: "donne-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.DONNE]
-            },
-            "INDOVINELLI": {
-                page: "indovinelli-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.INDOVINELLI]
-            },
-            "POLITICA": {
-                page: "politica-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.POLITICA]
-            },
-            "PROFESSIONI": {
-                page: "professioni-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.PROFESSIONI]
-            },
-            "RELIGIONE": {
-                page: "religione-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.RELIGIONE]
-            },
-            "SCUOLA": {
-                page: "scuola-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.SCUOLA]
-            },
-            "TECNOLOGIA": {
-                page: "tecnologia-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.TECNOLOGIA]
-            },
-            "UOMINI": {
-                page: "uomini-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.UOMINI]
-            },
-            "VARIE": {
-                page: "varie-{{PAGE}}.html",
-                categories: [Scraper.CATEGORIES.VARIE]
-            }
+            page: "aforismi-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.AFORISMI]
+        }, {
+            page: "animali-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.ANIMALI]
+        }, {
+            page: "calcio-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.CALCIO]
+        }, {
+            page: "carabinieri-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.CARABINIERI]
+        }, {
+            page: "colmi-e-freddure-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.COLMI, Scraper.CATEGORIES.FREDDURE]
+        }, {
+            page: "dal-dottore-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.DAL_DOTTORE]
+        }, {
+            page: "donne-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.DONNE]
+        }, {
+            page: "indovinelli-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.INDOVINELLI]
+        }, {
+            page: "politica-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.POLITICA]
+        }, {
+            page: "professioni-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.PROFESSIONI]
+        }, {
+            page: "religione-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.RELIGIONE]
+        }, {
+            page: "scuola-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.SCUOLA]
+        }, {
+            page: "tecnologia-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.TECNOLOGIA]
+        }, {
+            page: "uomini-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.UOMINI]
+        }, {
+            page: "varie-{{PAGE}}.html",
+            categories: [Scraper.CATEGORIES.VARIE]
         }]
     }
 
@@ -78,6 +62,7 @@ class FuoriDiTesta extends Scraper {
         const html = await this._downloadPage(page);
         const $ = cheerio.load(html);
         const divs = $("body > form > div.container > div.content > div:nth-child(3) > .end").prev("div");
+        if (!divs) throw new Error("No joke found");
         const jokes = Array.from(divs)
             .map(div => div.children[0].data)
             .map(joke => ({
